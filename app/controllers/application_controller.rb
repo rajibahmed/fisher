2ea5c169fb7272bd9f8b90ebda563577b02daa4d
Rajib Ahmed <l.rajibahmed@gmail.com>
2016-09-11 16:59:47 +0200

    Add basic authentication.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,25 @@ class ApplicationController < ActionController::API
 
   before_action :authenticate
 
-  def authenticate
-    puts request.headers.inspect
+  protected
+  def current_user
+    auth_param
   end
+
+  def auth_param
+    request.headers['AUTHORIZATION'].split(' ')[-1] || ""
+  end
+
+  def check_header
+    request.headers.include?('AUTHORIZATION')
+  end
+
+  def authenticate
+    check_header || render_unauthorized
+  end
+
+  def render_unauthorized
+    render json: 'Bad credentials', status: 401
+  end
+
 end
--- app/controllers/catches_controller.rb
+++ app/controllers/catches_controller.rb
@@ -1,13 +1,27 @@
 class CatchesController < ApplicationController
   def index
-    render json: Catch.all
+    render json: Catch.where(email: current_user)
   end
 
   def show
-    render nothing: true
+    katch = Catch.find(params[:id])
+    render json: katch, status: :created
+  rescue ActiveRecord::RecordNotFound
+    render json: Errors.for(:not_found), status: :not_found
   end
 
   def create
-    @catch = Catch.new(catch_params)
+    katch = Catch.new(catch_params)
+
+    if katch.save
+      render json: katch, status: :created
+    else
+      render json: Error.for(katch), status: :unprocessable_entity
+    end
+  end
+
+  private
+  def catch_params
+    params.permit(:email, :height, :lenght, :species, :lang, :lat)
   end
 end
