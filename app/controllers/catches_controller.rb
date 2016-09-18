dda95ff6329ec04ccdbeb1e5313d1a6992577fe2
Rajib Ahmed <l.rajibahmed@gmail.com>
2016-09-18 22:33:47 +0200

    Add specs for create catches.

--- app/controllers/catches_controller.rb
+++ app/controllers/catches_controller.rb
@@ -3,25 +3,18 @@ class CatchesController < ApplicationController
     render json: Catch.where(email: current_user)
   end
 
-  def show
-    katch = Catch.find(params[:id])
-    render json: katch, status: :created
-  rescue ActiveRecord::RecordNotFound
-    render json: Errors.for(:not_found), status: :not_found
-  end
-
   def create
     katch = Catch.new(catch_params)
 
     if katch.save
       render json: katch, status: :created
     else
-      render json: Error.for(katch), status: :unprocessable_entity
+      render json: Errors.for(:validation_failed, model: katch), status: :unprocessable_entity
     end
   end
 
   private
   def catch_params
-    params.permit(:email, :height, :lenght, :species, :lang, :lat)
+    params.require(:catch).permit(:email, :weight, :length, :species, :lang, :lat)
   end
 end
--- spec/controllers/catches_controller_spec.rb
+++ spec/controllers/catches_controller_spec.rb
@@ -12,12 +12,33 @@ RSpec.describe CatchesController, type: :controller do
     it "shows all catches for a user" do
       ca =  create(:catch)
       request.headers[:authorization] = "Bearer #{ca.email}"
+
       get :index
-    
-      puts response_json.inspect
+
       expect(response_json).not_to be_empty
       expect(response_json.size).to equal(1)
       expect(response_json.first[:email]).to eq("example@gmail.com")
+    end
+  end
+
+  context "POST /catches" do
+    it "creates a catch when params are valid" do
+      katch = attributes_for(:catch)
+      request.headers[:authorization] = "Bearer #{katch[:email]}"
+
+      post :create, params: {catch: katch}
+
+      expect(response.status).to eq(201)
+      expect(response_json[:species]).to eq("fish1")
+    end
+
+    it "show errors on validation error" do
+      katch = { email: "example@gmal.com" }
+      request.headers[:authorization] = "Bearer #{katch[:email]}"
+
+      post :create, params: {catch: katch}
+
+      expect(response.status).to eq(422)
     end
   end
 end
