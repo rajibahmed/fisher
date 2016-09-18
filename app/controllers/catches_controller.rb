1636fea47ef46136f435944e56e03bc406da5292
Rajib Ahmed <l.rajibahmed@gmail.com>
2016-09-19 01:02:37 +0200

    Change method name.

--- app/controllers/catches_controller.rb
+++ app/controllers/catches_controller.rb
@@ -7,7 +7,7 @@ class CatchesController < ApplicationController
     katch = Catch.new(catch_params)
 
     if katch.save
-      katch.create_thumbnails
+      katch.create_images
       render json: katch, status: :created
     else
       render json: Errors.for(:validation_failed, model: katch), status: :unprocessable_entity
