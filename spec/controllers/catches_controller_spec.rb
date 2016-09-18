ba3521b78fe3eccbfd9c8823247fea4bd4911d68
Rajib Ahmed <l.rajibahmed@gmail.com>
2016-09-18 21:34:36 +0200

    Add passing catches rest index spec.

--- spec/controllers/catches_controller_spec.rb
+++ spec/controllers/catches_controller_spec.rb
@@ -2,13 +2,22 @@ require 'rails_helper'
 
 RSpec.describe CatchesController, type: :controller do
   context "GET /catches" do
+    it "show errors when unauthorized" do
+      get :index
+
+      expect(response.status).to eq(401)
+      expect(response_json).not_to be_empty
+    end
+
     it "shows all catches for a user" do
       ca =  create(:catch)
-      get :index,nil,{ 'HTTP_AUTHORIZATION' =>  'Bearer example@gmail.com'}
-
+      request.headers[:authorization] = "Bearer #{ca.email}"
+      get :index
+    
+      puts response_json.inspect
       expect(response_json).not_to be_empty
       expect(response_json.size).to equal(1)
-      expect(response_json.first["email"]).to equal("example@gmail.com")
+      expect(response_json.first[:email]).to eq("example@gmail.com")
     end
   end
 end
